f4c2b6d043dc0c362534738fcb776e0dd90d3241
Carlos Rodriguez-Saltos <carodr5@emory.edu>
2018-06-07 13:40:27 -0400

    First commit

---
+++ R/r2final.R
@@ -0,0 +1,10 @@
+#' Get R2 values between preference to hear tutor song throuhgout development
+#' and quality of imitation of that song in the adult pupil
+#'
+#' @keywords operant conditioning, song development
+#' @details
+#' @export
+
+r2final <- function(){
+
+}
